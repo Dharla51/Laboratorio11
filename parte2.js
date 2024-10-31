de86563f5232da7bee8becab31ddaa83c9151b63
Dharla51 <dharlajduran@unicesar.edu.co>
2024-10-30 20:36:23 -0500

    parte 2 listo

--- parte2.js
+++ parte2.js
@@ -1,1 +1,8 @@
-
+let Num= prompt("DIGITE UN NUMERO: ");
+if (Num%2==0){
+    console.log("ES UN NUMERO PAR"); 
+    document.write("ES UN NUMERO PAR <br>");
+} else{
+    console.log("ES UN NUMERO IMPAR");
+    document.write("ES UN NUMERO IMPAR <br>"); 
+}
