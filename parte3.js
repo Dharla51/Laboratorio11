4a593f3d60f1a7b06fedc52bd6f00ff76bf03844
Dharla51 <dharlajduran@unicesar.edu.co>
2024-10-30 20:47:50 -0500

    parte 3 lista

--- parte3.js
+++ parte3.js
@@ -1,1 +1,16 @@
+let Monto= prompt("DIGITE SU MONTO: ");
+if(Monto>100){
+    let Desc= Monto-Monto*0.1; 
+    console.log("SE APLICO UN DESCUENTO DEL 10%"); 
+    console.log("EL MONTO FINAL ES: "+Desc);
+    document.write("SE APLICO UN DESCUENTO DEL 10% <br>"); 
+    document.write("EL MONTO FINAL ES: "+Desc+"<br>");
+}else{
+    if (Monto<=100){
+        console.log("NO SE APLICO DESCUENTO"); 
+        console.log("EL MONTO FINAL ES: "+Monto); 
+        document.write("NO SE APLICO DESCUENTO <br>");
+        document.write("EL MONTO FINAL ES:" +Monto+"<br>");
+    }
+}
 
