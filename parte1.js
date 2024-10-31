7a730335947a8b6f1c7e3c6c814e9f65612a477c
Dharla51 <dharlajduran@unicesar.edu.co>
2024-10-30 20:30:41 -0500

    parte 1 listo

--- parte1.js
+++ parte1.js
@@ -1,1 +1,15 @@
-
+let Nota= prompt("DIGITE SU NOTA: ");
+if (Nota>=90){
+   console.log("EXCELENTE NOTA, APROBADO CON HONORES");
+   document.write("EXCELENTE NOTA, APROBADO CON HONORES <br>");
+} else{
+   if (Nota>=70 && Nota<90){
+       console.log("APROBADO"); 
+       document.write("APROBADO <br>");
+   } else{
+   if(Nota<70){
+       console.log("REPROBADO"); 
+       document.write("REPROBADO <br>"); 
+       }
+   }
+}
