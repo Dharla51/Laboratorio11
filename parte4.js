8af1274a2c4d206f2062a4c218ca4590cc9abe0d
Dharla51 <dharlajduran@unicesar.edu.co>
2024-11-04 20:26:51 -0500

    parte 4 listo

--- parte4.js
+++ parte4.js
@@ -1,1 +1,11 @@
+const NumAle = Math.floor(Math.random() * 10) + 1;
+let Num = parseInt(prompt("DIGITA EL NUMERO:  "));
 
+if (Num === NumAle) {
+console.log("¡FELICIDADES, ADIVINASTE EL NUMERO!");
+document.write("¡FELICIDADES, ADIVINASTE EL NUMERO!");
+} 
+else {
+console.log("LO SIENTO, EL NUMERO ERA " + NumAle);
+document.write("LO SIENTO, EL NUMERO ERA " + NumAle);
+}
